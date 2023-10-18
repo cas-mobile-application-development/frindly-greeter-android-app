94e0c3de59993a29214718e6855d904f82d0616c
Yves Bonjour <yves.bonjour@rethinc.ch>
2023-10-18 11:39:13 +0200

    Adds hello world app

---
+++ app/src/main/java/ch/bfh/cas/mad/frindlygreeter/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.bfh.cas.mad.frindlygreeter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
