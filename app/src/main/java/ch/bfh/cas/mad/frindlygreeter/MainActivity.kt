e6f3313eddcce7008912c7560bf790a3ba13016d
Yves Bonjour <yves.bonjour@rethinc.ch>
2023-10-18 14:06:43 +0200

    Implements greet button

--- app/src/main/java/ch/bfh/cas/mad/frindlygreeter/MainActivity.kt
+++ app/src/main/java/ch/bfh/cas/mad/frindlygreeter/MainActivity.kt
@@ -1,12 +1,27 @@
 package ch.bfh.cas.mad.frindlygreeter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/***
+ * This code is not production ready code as it
+ * uses the most simple version for educational purposes.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonGreet = findViewById<Button>(R.id.button_greet)
+        val edittextName = findViewById<EditText>(R.id.edittext_name)
+        val textViewOutput = findViewById<TextView>(R.id.text_output)
+
+        buttonGreet.setOnClickListener {
+            textViewOutput.text = "Hi ${edittextName.text}!"
+        }
     }
 }
